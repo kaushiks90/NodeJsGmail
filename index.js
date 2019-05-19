c1d354727953387f16db6063f37ba83dd334e0a7
Kaushik <Kaushiks90@gmail.com>
2019-05-19 21:35:35 +0530

    Server changed

--- index.js
+++ index.js
@@ -70,4 +70,5 @@ app.post("/sendEmail", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server started..."));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
