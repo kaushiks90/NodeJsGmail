c9cfe84288204a2aa2f046863a8d0ca52fd72da9
Kaushik <Kaushiks90@gmail.com>
2019-05-21 16:06:37 +0530

    Cors

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
 const config = require('./config/config');
+var cors = require('cors');
 const app = express();
+
+//Cors Middleware
+app.use(cors());
 
 // Body Parser Middleware
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+	bodyParser.urlencoded({
+		extended: false
+	})
 );
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json("Test Message");
+app.get('/', (req, res) => {
+	res.json('Test Message');
 });
 
 app.use(express.static(__dirname));
-app.get("/download", (req, res) => {
-  var resume = path.join(__dirname, "public", "Resume.docx");
-  res.download(resume);
+app.get('/download', (req, res) => {
+	var resume = path.join(__dirname, 'public', 'Resume.docx');
+	res.download(resume);
 });
 
-app.post("/sendEmail", (req, res) => {
-  const output = `
+app.post('/sendEmail', (req, res) => {
+	const output = `
       <p>You have a new contact request</p>
       <h3>Contact Details</h3>
       <ul>  
@@ -36,42 +40,41 @@ app.post("/sendEmail", (req, res) => {
       <h3>Message</h3>
       <p>${req.body.message}</p>
     `;
-  var transporter = nodemailer.createTransport({
+	var transporter = nodemailer.createTransport({
+		host: config.transporter.host,
+		port: config.transporter.port,
+		secure: config.transporter.secure,
+		auth: {
+			user: config.auth.user,
+			pass: config.auth.password
+		}
+	});
 
-    host: config.transporter.host,
-    port: config.transporter.port,
-    secure: config.transporter.secure,
-    auth: {
-      user: config.auth.user,
-      pass: config.auth.password
-    }
-  });
+	// setup email data with unicode symbols
+	let mailOptions = {
+		from: config.mailOptions.from,
+		to: config.mailOptions.to,
+		subject: config.mailOptions.subject,
+		text: 'Hello world?',
+		html: output
+	};
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: config.mailOptions.from,
-    to: config.mailOptions.to,
-    subject: config.mailOptions.subject,
-    text: "Hello world?",
-    html: output
-  };
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message sent: %s', info.messageId);
+		console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message sent: %s", info.messageId);
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-    // res.send({
-    //   Success: config.messages.success
-    // });
-  });
-  res.send({
-    Success: config.messages.success
-  });
+		// res.send({
+		//   Success: config.messages.success
+		// });
+	});
+	res.send({
+		Success: config.messages.success
+	});
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
