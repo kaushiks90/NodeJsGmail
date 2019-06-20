9d295c4549caedac9e4dcd32ec390ac03caf1a42
kaushiks90 <kaushiks90@gmail.com>
2019-06-20 23:57:44 +0530

    production env

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.REACT_APP_ENV === 'production') {
     module.exports = require('./keys_prod')
 } else {
     module.exports = require('./keys_dev')
